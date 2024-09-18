33e6671adcf7e705dcf3cff86f89cee938c6d0d2
23DP2KVecm <166109779+23DP2KVecm@users.noreply.github.com>
2024-09-18 10:07:32 +0000

    Speeding Ticket!

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -9,21 +9,11 @@ public class App
     {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Give the first number:");
-        int first = Integer.valueOf(scanner.nextLine());
-
-        System.out.println("Give second number:");
-        int second = Integer.valueOf(scanner.nextLine());
-
-        int sumof = first + second;
-        int divof = first - second;
-        int multof = first * second;
-        double quof = first / (double) second;
-
-        System.out.println(first + " + " + second + " = " + sumof);
-        System.out.println(first + " - " + second + " = " + divof);
-        System.out.println(first + " * " + second + " = " + multof);
-        System.out.println(first + " / " + second + " = " + quof);
+        System.out.println("Give the speed:");
+        int speed = Integer.valueOf(scanner.nextLine());
         
+        if (speed>120) {
+            System.out.println("Speeding ticket!");
+        }
     }
 }

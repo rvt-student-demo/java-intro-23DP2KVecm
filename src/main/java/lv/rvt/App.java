57a859fc957040ac93f1ec0aca69f338f0ab1125
23DP2KVecm <166109779+23DP2KVecm@users.noreply.github.com>
2024-09-19 10:28:42 +0000

    Only positives

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -3,40 +3,21 @@ package lv.rvt; // Klases atrašanās vieta projektā
 import java.util.*;
 
 public class App
-
 {
         public static void main(String[] args) {
+        int mult = 0;
         Scanner scan = new Scanner(System.in);
-        System.out.println("Value of the gift?");
-        int sum = scan.nextInt();
-        double taxAmount = 0;
-
-        if (sum < 5000) {
-            System.out.println("No tax!");
-        }
-        if (sum >= 5000 && sum < 25000) {
-            taxAmount = 100 + (sum - 5000) * 0.08;
-            System.out.println("Tax: " + taxAmount);
-
-        }
-        if (sum >= 25000 && sum < 55000) {
-            taxAmount = 1700 + (sum - 25000) * 0.1;
-            System.out.println("Tax: " + taxAmount);
-
-        }
-        if (sum >= 55000 && sum < 200000) {
-            taxAmount = 4700 + (sum - 55000) * 0.12;
-            System.out.println("Tax: " + taxAmount);
-
-        }
-        if (sum >= 200000 && sum < 1000000) {
-            taxAmount = 22100 + (sum - 200000) * 0.15;
-            System.out.println("Tax: " + taxAmount);
-
-        }
-        if (sum >= 1000000) {
-            taxAmount = 142100 + (sum - 1000000) * 0.17;
-            System.out.println("Tax: " + taxAmount);
+        System.out.println("Give a number:");
+        int num = scan.nextInt();
+        while (num != 0) {
+            if (num > 0) {
+                System.out.println(num * num);
+            }
+            if (num < 0) {
+                System.out.println("Unsuitable number");
+            }
+            System.out.println("Give a number");
+            num = scan.nextInt();
         }
     }
 }

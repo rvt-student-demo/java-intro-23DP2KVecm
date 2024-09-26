e1d1f4f6531a949ba0029b624f7aee83e58b0fa4
23DP2KVecm <166109779+23DP2KVecm@users.noreply.github.com>
2024-09-26 10:21:48 +0000

    Repeating, breaking and remembering (5 parts)

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -6,14 +6,31 @@ public class App
 {
         public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        System.out.println("Give a number:");
+        int sum = 0;
+        int numbers = 0;
+        double avg = 0;
+        int even = 0;
+        int odd = 0;
+        System.out.println("Give numbers:");
         int n = scan.nextInt();
-        int num = 1;
-        int factorial = 1;
-        while (num<=n) {
-            factorial *= num;
-            num++;
-            System.out.println(factorial);
+        while (n!=-1) {
+            if (n % 2 == 0){
+                even += 1;
+            }
+            else {
+                odd += 1;
+            }
+            sum = sum + n;
+            int num = scan.nextInt();
+            n = num;
+            numbers += 1;
         }
+        avg = sum / numbers;
+        System.out.println("Thx! Bye!");
+        System.out.println("Sum: " + sum);
+        System.out.println("Numbers: " + numbers);
+        System.out.println("Average: " + avg);
+        System.out.println("Even: " + even);
+        System.out.println("Odd: " + odd);
     }
 }

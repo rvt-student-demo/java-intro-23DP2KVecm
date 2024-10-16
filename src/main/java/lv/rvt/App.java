4a78ad782839aa5765a0c31c5dcb2181de648818
23DP2KVecm <166109779+23DP2KVecm@users.noreply.github.com>
2024-10-16 10:29:26 +0000

    Second plus third

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -5,9 +5,19 @@ import java.util.*;
 public class App
 {
         public static void main(String[] args) {
-        Stars.printStars(3);
-        Stars.printSquare(4);
-        Stars.printRectangle(17, 3);
-        Stars.printTriangle(4);
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<Integer> list = new ArrayList<>();
+        while(true) {
+                int num = scanner.nextInt();
+                if (num==0){
+                        break;
+                }
+                list.add(num);
+        }
+        int sum = 0;
+        for (int number : list){
+                sum += number;
+        }
+        System.out.println(sum);
         }
 }

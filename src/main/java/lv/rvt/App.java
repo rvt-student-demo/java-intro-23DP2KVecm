ceb8bee1e8efb1b33e7d257a0960889fff1623f7
23DP2KVecm <166109779+23DP2KVecm@users.noreply.github.com>
2024-10-24 10:21:31 +0000

    Viss saglabats

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -2,52 +2,17 @@ package lv.rvt; // Klases atrašanās vieta projektā
 
 import java.util.*;
 import java.io.*;
-
+import lv.rvt.Account;
+import lv.rvt.Person;
+import lv.rvt.Room;
+import lv.rvt.Product;
 public class App
 {
-        public static void main(String[] args) {
-                Scanner scanner = new Scanner(System.in);
-                Random random = new Random();
-                System.out.println("I am thinking of a number from 1 to 10.");
-                System.out.println("You must guess what it is in three tries.");
-                int uzvaras = 0;
-                for (int i = 0; i < 10; i++){
-                        int randInt = random.nextInt(10) + 1;
-                        int ciklsWhile = 0;
-                        while (ciklsWhile != 3){
-                                System.out.print("Enter a guess: ");
-                                int num = scanner.nextInt();
-                                if (num != randInt){
-                                        if (num <= randInt-3 || num >= randInt+3){
-                                                System.out.println("cold");
-                                        }
-                                        if (num == randInt-2 || num == randInt+2){
-                                                System.out.println("warm");
-                                        }
-                                        if (num == randInt-1 || num == randInt+1){
-                                                System.out.println("hot");
-                                        }
-                                }
-                                else {
-                                        uzvaras++;
-                                        System.out.println("RIGHT!");
-                                        System.out.println("You have won " + uzvaras + " out of " + i + " rounds.");
-                                        
-                                }
-                        ciklsWhile++;
-                        }
-                }
-                if (uzvaras <= 7){
-                        System.out.println("Your rating: amateur.");
-                }
-                if (uzvaras == 8){
-                        System.out.println("Your rating: advanced.");
-                }
-                if (uzvaras == 9){
-                        System.out.println("Your rating: proffesional   .");
-                }
-                if (uzvaras == 10){
-                        System.out.println("Your rating: amateur.");
-                }
+        public static void main(String[] args) 
+        {
+                Product banana = new Product("banana", 1.1, 13);
+                banana.printProduct();
+                
         }
+
 }
--- src/main/java/lv/rvt/Stars.java
+++ src/main/java/lv/rvt/Stars.java
@@ -9,26 +9,24 @@ public class Stars {
         
     }
     //Jūsu metode būs šeit
-    public static void printSquare(int size){
-        for (int b = 0; b < size; b++) {
-            System.out.println();
-            printStars(size);
-            
+    public static void printSpaces(int number) {
+        //Code goes here
+        for (int i = 0; i < number; i++){
+        System.out.print(" ");
         }
+        
     }
-
-    public static void printRectangle(int width, int height) {
-        for (int c = 0; c < height; c++){
-            System.out.println();
-            printStars(width);
-            
-        }
-    }
-
     public static void printTriangle(int size) {
-        for (int d = 0; d <= size; d++){
-            printStars(d);
-            System.out.println();
+        //Code goes here
+        for (int i = 0; i < size; i++){
+            int m = i;
+            if (m == size-i) {
+                printStars(i);
+            }
+            else {
+                printSpaces(1);
+            }
         }
+        
     }
 }

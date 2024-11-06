1f3bcf1ab01b3853debd3e1e646ce0cfce2ae28c
23DP2KVecm <166109779+23DP2KVecm@users.noreply.github.com>
2024-11-06 12:06:43 +0000

    Person klases saglabasana

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -2,29 +2,30 @@ package lv.rvt; // Klases atrašanās vieta projektā
 
 import java.util.*;
 import java.io.*;
-import lv.rvt.Account;
 import lv.rvt.Person;
 import lv.rvt.Room;
 import lv.rvt.Product;
+import lv.rvt.Agent;
 public class App
 {
-        public static void main(String[] args) {
-                DecreasingCounter counter = new DecreasingCounter(100);
-        
-                counter.printValue();
-        
-                counter.decrement();
-                counter.printValue();
-        
-                counter.decrement();
-                counter.printValue();
-                
-                counter.reset();
-                counter.printValue();
+    public static void main(String[] args) {
+        Person matti = new Person("Matti");
+        Person juhana = new Person("Juhana");
+        Person es = new Person("Klavs");
 
-                counter.decrement();
-                counter.printValue();
-
-            }
-
+        es.setHeight(192);
+        es.setWeight(68.5);
+        es.setAge(16);
+        System.out.println(es.getName() + ", body mass index is " + es.bodyMassIndex());
+        System.out.println(es.toString());
+    
+        matti.setHeight(180);
+        matti.setWeight(86);
+    
+        juhana.setHeight(175);
+        juhana.setWeight(64);
+    
+        System.out.println(matti.getName() + ", body mass index is " + matti.bodyMassIndex());
+        System.out.println(juhana.getName() + ", body mass index is " + juhana.bodyMassIndex());
+}
 }
--- src/main/java/lv/rvt/Person.java
+++ src/main/java/lv/rvt/Person.java
@@ -1,15 +1,41 @@
 package lv.rvt;
 import java.util.*;
+
 public class Person {
     private String name;
     private int age;
+    private double weight;
+    private int height;
 
-    //Class constructor
     public Person(String initialName) {
-        this.name = initialName;
         this.age = 0;
+        this.weight = 0;
+        this.height = 0;
+        this.name = initialName;
     }
-    public void printPerson() {
-        System.out.println(this.name + ", age " + this.age + " years");
+
+    public String getName() {
+        return this.name;
+    }
+
+
+    public void setHeight(int newHeight) {
+        this.height = newHeight;
+    }
+
+    public void setAge(int newAge) {
+        this.age = newAge;
+    }
+
+    public void setWeight(double newWeight) {
+        this.weight = newWeight;
+    }
+
+    public double bodyMassIndex() {
+        double heigthPerHundred = this.height / 100.0;
+        return this.weight / (heigthPerHundred * heigthPerHundred);
+    }
+    public String toString() {
+        return this.name + ", age " + this.age + " years, my body mass index is " + this.bodyMassIndex();
     }
 }

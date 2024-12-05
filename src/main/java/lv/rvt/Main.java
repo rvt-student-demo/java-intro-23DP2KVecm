f3371e994299695d90e14d004f60ed6aa0f81481
23DP2KVecm <166109779+23DP2KVecm@users.noreply.github.com>
2024-12-05 12:55:59 +0000

    HealthStation p3

--- src/main/java/lv/rvt/Main.java
+++ src/main/java/lv/rvt/Main.java
@@ -8,14 +8,24 @@ import lv.rvt.Person;
 
 public class Main {
     public static void main(String[] args) {
-        // example main program for the first section of the exercise
-    
+
         HealthStation childrensHospital = new HealthStation();
     
         Person ethan = new Person("Ethan", 1, 110, 7);
         Person peter = new Person("Peter", 33, 176, 85);
     
-        System.out.println(ethan.getName() + " weight: " + childrensHospital.weight(ethan) + " kilos");
-        System.out.println(peter.getName() + " weight: " + childrensHospital.weight(peter) + " kilos");
-    }   
+        System.out.println("weighings performed: " + childrensHospital.weighings());
+    
+        childrensHospital.weight(ethan);
+        childrensHospital.weight(peter);
+    
+        System.out.println("weighings performed: " + childrensHospital.weighings());
+    
+        childrensHospital.weight(ethan);
+        childrensHospital.weight(ethan);
+        childrensHospital.weight(ethan);
+        childrensHospital.weight(ethan);
+    
+        System.out.println("weighings performed: " + childrensHospital.weighings());
+    } 
 }
